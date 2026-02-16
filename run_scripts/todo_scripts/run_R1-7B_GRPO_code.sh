1a6b3ad075fbacd2dfe59ffbfbe571f5e9224878
1BIMU <2412271255@qq.com>
2026-02-16 16:48:17 +0800

    added code

---
+++ run_scripts/todo_scripts/run_R1-7B_GRPO_code.sh
@@ -0,0 +1,61 @@
+set -x
+
+source uv_verl/bin/activate
+export CONDA_BIN_PATH=$(dirname $(which python3))
+
+train_path=data/code_processed.parquet
+
+train_files="['$train_path']"
+val_files="['data/offline_eval/code__mbpp.parquet']"
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    algorithm.use_kl_in_reward=False \
+    data.train_files="$train_files" \
+    data.val_files="$val_files" \
+    data.train_batch_size=512 \
+    data.shuffle=True \
+    data.trust_remote_code=True \
+    data.max_prompt_length=2048 \
+    data.max_response_length=16384 \
+    data.filter_overlong_prompts=False \
+    data.truncation='left' \
+    actor_rollout_ref.model.path=deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.total_training_steps=200 \
+    actor_rollout_ref.actor.optim.warmup_style=constant \
+    actor_rollout_ref.actor.optim.weight_decay=0.1 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0 \
+    actor_rollout_ref.actor.use_torch_compile=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.use_dynamic_bsz=False \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.rollout.do_sample=True \
+    actor_rollout_ref.rollout.temperature=0.6 \
+    actor_rollout_ref.rollout.val_kwargs.do_sample=True \
+    actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
+    actor_rollout_ref.rollout.max_model_len=24576 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=49152 \
+    actor_rollout_ref.rollout.enforce_eager=True \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    trainer.critic_warmup=0 \
+    trainer.logger='["console","wandb"]' \
+    trainer.project_name='SPPO' \
+    trainer.experiment_name='R1-7B_GRPO_code' \
+    trainer.default_local_dir='./' \
+    trainer.n_gpus_per_node=4 \
+    trainer.nnodes=1 \
+    trainer.save_freq=10 \
+    trainer.test_freq=2 \
+    trainer.val_before_train=False \
+    trainer.total_training_steps=200 $@
