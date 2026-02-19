4f983b35beaa7d90a1337bf08ea6188fa74b15f1
BIMU <2412271255@qq.com>
2026-02-19 22:46:42 +0800

    ebtech

--- uv_verl.sh
+++ uv_verl.sh
@@ -1,5 +1,8 @@
 
 set -ex
+source /public/bin/network_accelerate
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source /public/bin/network_accelerate_stop
 export PATH="$HOME/.local/bin:$PATH"
 uv venv uv_verl --python 3.10
 source uv_verl/bin/activate
